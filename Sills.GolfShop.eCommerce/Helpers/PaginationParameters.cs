d05ed47b637c9b1b29bacebd46769731fcf54894
David Sills <david.sills100@gmail.com>
2026-04-07 08:14:45 -0500

    working on pagination.

---
+++ Sills.GolfShop.eCommerce/Helpers/PaginationParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace Sills.GolfShop.eCommerceAPI.Helpers
+{
+    public class PaginationParameters
+    {
+        const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+    }
+}
