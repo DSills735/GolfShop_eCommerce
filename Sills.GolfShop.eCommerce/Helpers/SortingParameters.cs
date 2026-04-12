c63c73ba655f7ff545a72f329ff9f4a0c0a90ce1
David Sills <david.sills100@gmail.com>
2026-04-12 13:36:39 -0500

    sorting params fin

--- Sills.GolfShop.eCommerce/Helpers/SortingParameters.cs
+++ Sills.GolfShop.eCommerce/Helpers/SortingParameters.cs
@@ -1,13 +1,21 @@
 ﻿namespace Sills.GolfShop.eCommerceAPI.Helpers
 {
-    public class SortingParameters
+    public class ProductParameters
     {
         public string name { get; set; }
         public decimal? minPrice { get; set; }
         public decimal? maxPrice { get; set; }
 
         public string? sortBy { get; set; } = null;
+    }
 
-
+    public class CategoryParameters
+    {
+        public string name { get; set; }
+        public string? sortBy { get; set; } = null;
+    }
+     public class SalesParameters
+    {
+        public string? sortBy { get; set; } = null;
     }
 }
