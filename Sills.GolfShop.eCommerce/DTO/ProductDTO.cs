b96d4a016f71715c1c61be61742c7d4dafec5ffb
David Sills <david.sills100@gmail.com>
2026-04-14 09:52:11 -0500

    working on object mapping

---
+++ Sills.GolfShop.eCommerce/DTO/ProductDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Sills.GolfShop.eCommerceAPI.DTO
+{
+    public record ProductDto
+    {
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public int QuantityInStock { get; init; }
+    }
+}
---
+++ Sills.GolfShop.eCommerce/Mapping/ProductMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace Sills.GolfShop.eCommerceAPI.Mapping
+{
+    public static class ProductMapper
+    {
+        public static DTO.ProductDTO ToDTO(this Models.Product product)
+        {
+            if (product == null) return null;
+            return new DTO.ProductDTO
+            {
+                Name = product.Name,
+                Description = product.Description,
+                QuantityInStock = product.QuantityInStock
+            };
+        }
+    }
+}
